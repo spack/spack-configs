1b24da66a86f54080616d7e6fea2d3f73600d7db
Frank Willmore <willmore@anl.gov>
2021-10-01 21:44:01 +0000

    fix clone path

--- ANL/JLSE/Arcticus/E4S-21.05/setclonepath.sh
+++ ANL/JLSE/Arcticus/E4S-21.05/setclonepath.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export RAND=$(python -c "import string; import random; print(''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase) for _ in range(8)))")
-export SPACK_CLONE_PATH=$CFS/m3503/spack-${RAND}
+#export SPACK_CLONE_PATH=$CFS/m3503/spack-${RAND}
+export SPACK_CLONE_PATH=spack-${RAND}
