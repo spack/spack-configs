8ada01cdfc97525560540a86aaf8a50480e87f1f
Frank Willmore <willmore@anl.gov>
2021-10-01 22:42:07 +0000

    trying /tmp

--- ANL/JLSE/Arcticus/E4S-21.05/setclonepath.sh
+++ ANL/JLSE/Arcticus/E4S-21.05/setclonepath.sh
@@ -2,4 +2,5 @@
 
 export RAND=$(python -c "import string; import random; print(''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase) for _ in range(8)))")
 #export SPACK_CLONE_PATH=$CFS/m3503/spack-${RAND}
-export SPACK_CLONE_PATH=/run/user/20661/spack-${RAND}
+#export SPACK_CLONE_PATH=/run/user/20661/spack-${RAND}
+export SPACK_CLONE_PATH=/tmp/spack-${RAND}
