e923a1ea2e6b65bafd0213438ee9c5e69b7daafd
Stephen Sachs <stesachs@amazon.com>
2022-10-04 14:36:58 +0200

    Add configuration for AWS ParallelCluster
    - This includes setup for skylake, icelake, zen3, and grv2 based clusters.
    - Includes pre-defined recipes for gromacs, quantum-espresso, openfoam, wrf,
    - lammps, mpas-model, and py-devito.
    - Use `postinstall.sh` in ParallelCluster cluster creation or
    - Run `curl -L https://raw.githubusercontent.com/spack/spack-configs/AWS/parallelcluster/postinstall.sh | sudo bash`
    - zen3 is recognized as zen2 by archspec, so using zen2 only.

---
+++ AWS/parallelcluster/postinstall.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -e
+
+##############################################################################################
+# # This script will setup Spack and best practices for a few applications.                  #
+# # Use as postinstall in AWS ParallelCluster (https://docs.aws.amazon.com/parallelcluster/) #
+##############################################################################################
+
+# Install onto first shared storage device
+. /etc/parallelcluster/cfnconfig || {
+    echo "Cannot find ParallelCluster configs"
+    echo "Installing Spack into /shared/spack for ec2-user."
+    cfn_ebs_shared_dirs="/shared"
+    cfn_cluster_user="ec2-user"
+}
+
+install_path=${SPACK_ROOT:-"${cfn_ebs_shared_dirs}/spack"}
+spack_branch="develop"
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+major_version() {
+    pcluster_version=$(grep -oE '[0-9]*\.[0-9]*\.[0-9]*' /opt/parallelcluster/.bootstrapped)
+    echo "${pcluster_version/\.*}"
+}
+
+# Make first user owner of Spack installation when script exits.
+fix_owner() {
+    rc=$?
+    if [ -z "${SPACK_ROOT}" ]
+    then
+        chown -R ${cfn_cluster_user}:${cfn_cluster_user} "${install_path}"
+    fi
+    exit $rc
+}
+trap "fix_owner" SIGINT EXIT
+
+download_spack() {
+    if [ -z "${SPACK_ROOT}" ]
+    then
+        [ -d ${install_path} ] || \
+            git clone https://github.com/spack/spack -b ${spack_branch} ${install_path}
+    fi
+}
+
+architecture() {
+    lscpu  | grep "Architecture:" | awk '{print $2}'
+}
+
+# zen3 EC2 instances (e.g. hpc6a) is misidentified as zen2 so zen3 packages are found under packages-zen2.yaml.
+target() {
+    (
+        . ${install_path}/share/spack/setup-env.sh
+        spack arch -t
+    )
+}
+
+set_pcluster_defaults() {
+    # Set versions of pre-installed software in packages.yaml
+    SLURM_VERSION=$(. /etc/profile && sinfo --version | cut -d' ' -f 2 | sed -e 's?\.?-?g')
+    LIBFABRIC_MODULE=$(. /etc/profile && module avail libfabric 2>&1 | grep libfabric | head -n 1 | sed -e 's?~??g' | xargs )
+    LIBFABRIC_MODULE_VERSION=$(. /etc/profile && module avail libfabric 2>&1 | grep libfabric | head -n 1 |  cut -d / -f 2 | sed -e 's?~??g' | xargs )
+    LIBFABRIC_VERSION=${LIBFABRIC_MODULE_VERSION//amzn*}
+    GCC_VERSION=$(gcc -v 2>&1 |tail -n 1| awk '{print $3}' )
+
+    # Write the above as actual yaml file and only parse the \$.
+    mkdir -p ${install_path}/etc/spack
+        curl -Ls https://raw.githubusercontent.com/spack/spack-configs/main/AWS/parallelcluster/packages-"$(target)".yaml -o ${scriptdir}/packages.yaml
+    eval "echo \"$(cat ${scriptdir}/packages.yaml)\"" > ${install_path}/etc/spack/packages.yaml
+
+    for f in mirrors modules config; do
+        aws s3 cp s3://spack-configs/AWS/parallelcluster/${f}.yaml  ${install_path}/etc/spack/${f}.yaml
+    done
+
+}
+
+setup_spack() {
+    cd "${install_path}"
+
+    # Load spack at login
+    if [ -z "${SPACK_ROOT}" ]
+    then
+        echo ". ${install_path}/share/spack/setup-env.sh" > /etc/profile.d/spack.sh
+        echo ". ${install_path}/share/spack/setup-env.csh" > /etc/profile.d/spack.csh
+    fi
+
+    . "${install_path}/share/spack/setup-env.sh"
+    spack compiler add --scope site
+    spack external find --scope site
+
+    # Remove all autotools/buildtools packages. These versions need to be managed by spack or it will
+    # eventually end up in a version mismatch (e.g. when compiling gmp).
+    spack tags build-tools | xargs -I {} spack config --scope site rm packages:{}
+    spack buildcache keys --install --trust
+}
+
+install_packages() {
+    . /etc/profile.d/spack.sh
+
+    # Compiler needed for all kinds of codes. It makes no sense not to install it.
+    if [ "x86_64" == "$(architecture)" ]
+    then
+        packages_yaml="$(spack config --scope site edit compilers --print-file)"
+        SPACK_OS=$(spack arch -o)
+        # Add oneapi@latest & intel@latest
+        STUB_ONEAPI_COMPILER=$(spack list --format version_json intel-oneapi-compilers-classic | jq -rc .[0].versions[] | sort | tail -n1 | awk '{print "intel@"$1}')
+        echo -e "- compiler:\n    target:     x86_64\n    operating_system:   ${SPACK_OS}\n    modules:    []\n    spec:       ${STUB_ONEAPI_COMPILER}\n    paths:\n      cc:       /usr/bin/true\n      cxx:      /usr/bin/true\n      f77:      /usr/bin/true\n      fc:       /usr/bin/true" \
+             >> "${packages_yaml}"
+        spack install intel-oneapi-compilers-classic %${STUB_ONEAPI_COMPILER} ^patchelf%gcc
+        # Remove stubs
+        head -n -10 "${packages_yaml}" > $$.tmp && mv $$.tmp "${packages_yaml}"
+        (
+            . "$(spack location -i intel-oneapi-compilers)/setvars.sh"
+            spack compiler add --scope site
+        )
+    fi
+
+    # Install any specs provided to the script.
+    for spec in "$@"
+    do
+        spack install "${spec}"
+    done
+}
+
+if [ "3" != "$(major_version)" ]; then
+    echo "ParallelCluster version $(major_version) not supported."
+    exit 1
+fi
+
+download_spack |& tee -a /var/log/spack-postinstall.log
+set_pcluster_defaults |& tee -a /var/log/spack-postinstall.log
+setup_spack |& tee -a /var/log/spack-postinstall.log
+install_packages "$@" |& tee -a /var/log/spack-postinstall.log
