7681f59723d2c9eaa7f04316afebc367297843be
Stephen Sachs <stesachs@amazon.com>
2022-11-09 13:48:47 +0100

    Use `SharedStorage` from cluster config file
    Get information about the first shared storage and the first user directly from
    the pcluster config file. This is more robust in case there is no shared EBS.

--- AWS/parallelcluster/postinstall.sh
+++ AWS/parallelcluster/postinstall.sh
@@ -9,7 +9,38 @@ set -e
 # TODO: Once https://github.com/archspec/archspec-json/pull/57 makes it into Spack we need to rename: graviton2 -> neoverse_n1, graviton3 -> neoverse_v1
 
 # Install onto first shared storage device
-. /etc/parallelcluster/cfnconfig || {
+cluster_config="/opt/parallelcluster/shared/cluster-config.yaml"
+[ -f "${cluster_config}" ] && {
+    os=$(python << EOF
+#/usr/bin/env python
+import yaml
+with open("${cluster_config}", 'r') as s:
+    print(yaml.safe_load(s)["Image"]["Os"])
+EOF
+      )
+
+    case "${os}" in
+        alinux*)
+            cfn_cluster_user="ec2-user"
+            ;;
+        centos*)
+            cfn_cluster_user="centos"
+            ;;
+        ubuntu*)
+            cfn_cluster_user="ubuntu"
+            ;;
+        *)
+            cfn_cluster_user=""
+    esac
+
+    cfn_ebs_shared_dirs=$(python << EOF
+#/usr/bin/env python
+import yaml
+with open("${cluster_config}", 'r') as s:
+    print(yaml.safe_load(s)["SharedStorage"][0]["MountDir"])
+EOF
+                       )
+} || . /etc/parallelcluster/cfnconfig || {
     echo "Cannot find ParallelCluster configs"
     echo "Installing Spack into /shared/spack for ec2-user."
     cfn_ebs_shared_dirs="/shared"
