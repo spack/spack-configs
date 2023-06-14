e98a69c541413978de5d3e07fcccc133302e06c2
Stephen Sachs <stesachs@amazon.com>
2023-06-14 11:01:58 +0200

    [AWS] Remove empty config.yaml

--- AWS/parallelcluster/postinstall.sh
+++ AWS/parallelcluster/postinstall.sh
@@ -172,9 +172,7 @@ set_pcluster_defaults() {
     ( download_packages_yaml "$(target)" )
     eval "echo \"$(cat /tmp/packages.yaml)\"" > ${install_path}/etc/spack/packages.yaml
 
-    for f in modules config; do
-        curl -Ls https://raw.githubusercontent.com/spack/spack-configs/main/AWS/parallelcluster/${f}.yaml -o ${install_path}/etc/spack/${f}.yaml
-    done
+    curl -Ls https://raw.githubusercontent.com/spack/spack-configs/main/AWS/parallelcluster/modules.yaml -o ${install_path}/etc/spack/modules.yaml
 }
 
 setup_spack() {
